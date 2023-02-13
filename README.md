e908cd7835b86e94c943b57b62efe49ab08ae318
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2023-02-13 13:49:01 +0200

    LCJS 4.0.1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JavaScript Animated Geographical Temperature Chart
 
-![JavaScript Animated Geographical Temperature Chart](geoChartUsaTemperature.png)
+![JavaScript Animated Geographical Temperature Chart](geoChartUsaTemperature-darkGold.png)
 
 This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
 
@@ -8,13 +8,13 @@ LightningChart JS is entirely GPU accelerated and performance optimized charting
 
 The demo can be used as an example or a seed project. Local execution requires the following steps:
 
-- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
-- Open the project folder in a terminal:
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
 
-        npm install              # fetches dependencies
-        npm start                # builds an application and starts the development server
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
 
-- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
 
 
 ## Description
@@ -25,7 +25,7 @@ This example displays a heat map data set of temperature grid measurements on th
 
 The data set used in this example has 500 columns (longitude) and 400 rows (latitude). This translates to roughly 12 km step between longitude measurements and 6 km step between latitude measurements. The total number of measurements is 200 000 data points.
 
-The visualization is done by laying a predefined picture of a map under the heatmap, which is styled with transparent colours. This approach is simple, but only suitable when the geographical view is predefined. 
+The visualization is done by laying a predefined picture of a map under the heatmap, which is styled with transparent colours. This approach is simple, but only suitable when the geographical view is predefined.
 
 LightningChart JS can also be combined with mapping solutions such as [Leaflet](https://leafletjs.com/), which allows the combination of interactive, dynamic maps and the data visualization power of LightningChart.
 
@@ -35,20 +35,20 @@ The picture used in this example is quite low resolution (1600 x 883 px), if a h
 
 This example also generates a random data set that is used to showcase the real-time capabilities of LightningChart JS - the heat map visualization is animated continuously with smooth 60 refresh rate. Traditionally, this kind of applications do not exist in web pages because it requires too much processing power and results in bad user experience. However, LightningCharts real-time performance oriented heat map features enable running this example as well as even more stressful usage cases on desktop, mobile and tablet devices while leaving plenty of resources for the rest of the web page.
 
-To learn more about the ground-breaking performance differences between LightningChart and traditional data visualization tools, please refer to our [performance comparison studies](https://www.arction.com/high-performance-javascript-charts/).
+To learn more about the ground-breaking performance differences between LightningChart and traditional data visualization tools, please refer to our [performance comparison studies](https://lightningchart.com/high-performance-javascript-charts/).
 
 **More map examples**:
 
-- [Data visualization over LightningChart JS Map Charts](https://www.arction.com/lightningchart-js-interactive-examples/examples/lcjs-example-1103-mapChartVizXY.html)
-- [Data visualization over Google Maps](https://blog.arction.com/easy-geospatial-data-visualization-with-lightningchart-js-and-google)
-- [LightningChart JS Map Charts with Drill-down](https://www.arction.com/lightningchart-js-interactive-examples/examples/lcjs-example-1111-covidDrillDownDashboard.html)
-- [Simple Countries Visualization](https://www.arction.com/lightningchart-js-interactive-examples/examples/lcjs-example-1101-mapChartDynamicColor.html)
+-   [Data visualization over LightningChart JS Map Charts](https://lightningchart.com/lightningchart-js-interactive-examples/examples/lcjs-example-1103-mapChartVizXY.html)
+-   [Data visualization over Google Maps](https://blog.arction.com/easy-geospatial-data-visualization-with-lightningchart-js-and-google)
+-   [LightningChart JS Map Charts with Drill-down](https://lightningchart.com/lightningchart-js-interactive-examples/examples/lcjs-example-1111-covidDrillDownDashboard.html)
+-   [Simple Countries Visualization](https://lightningchart.com/lightningchart-js-interactive-examples/examples/lcjs-example-1101-mapChartDynamicColor.html)
 
 # About LightningChart Heatmaps Performance
 
 Since September 2021, LightningChart JS is the proven performance leader in real-time and static heatmaps visualization in the web.
 
-At that time, we took the initiative to compare the performance of the different commercial data visualization tools as well as open source tools, who offer generic heatmap chart solutions. You can find the study description and results [here](https://www.arction.com/js-heatmaps-performance-comparison/).
+At that time, we took the initiative to compare the performance of the different commercial data visualization tools as well as open source tools, who offer generic heatmap chart solutions. You can find the study description and results [here](https://lightningchart.com/js-heatmaps-performance-comparison/).
 
 The most important finding from this study was that the maximum data visualization capacity of LightningChart JS greatly exceeds that of any other visualization library, being able to display more than **5 billion data points**. In context of this example, this translates to the latitude/longitude step between measurements that can be interpreted.
 
@@ -68,25 +68,25 @@ This is a crucial feature in real-time geographical data monitoring applications
 
 If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
 
-Official [API documentation][1] can be found on [Arction][2] website.
+Official [API documentation][1] can be found on [LightningChart][2] website.
 
 If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
 
-If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+If you think you found a bug in the LightningChart JavaScript library, please contact support@lightningchart.com.
 
-Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
 
 [0]: https://github.com/Arction/
-[1]: https://www.arction.com/lightningchart-js-api-documentation/
-[2]: https://www.arction.com
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
 [3]: https://stackoverflow.com/questions/tagged/lightningchart
-[4]: https://www.arction.com/support-services/
+[4]: https://lightningchart.com/support-services/
 
-© Arction Ltd 2009-2020. All rights reserved.
+© LightningChart Ltd 2009-2022. All rights reserved.
 
 
-[ColorRGBA]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#colorrgba
-[ImageFill]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/imagefill.html
-[ImageFitMode]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/enums/imagefitmode.html
-[LUT]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/lut.html
+[ColorRGBA]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/functions/ColorRGBA.html
+[ImageFill]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/ImageFill.html
+[ImageFitMode]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/enums/ImageFitMode.html
+[LUT]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/LUT.html
 
--- src/index.js
+++ src/index.js
@@ -3,177 +3,157 @@
  */
 
 // Import LightningChartJS
-const lcjs = require("@arction/lcjs");
-const xydata = require("@arction/xydata");
+const lcjs = require('@arction/lcjs')
+const xydata = require('@arction/xydata')
 
 // Extract required parts from LightningChartJS.
-const { 
-  lightningChart, 
-  ColorRGBA,
-  ImageFill,
-  ImageFitMode,
-  PalettedFill,
-  LUT,
-  formatLongitudeLatitude,
-  emptyLine,
-  Themes 
-} = lcjs;
+const { lightningChart, regularColorSteps, ImageFill, ImageFitMode, PalettedFill, LUT, formatLongitudeLatitude, emptyLine, Themes } = lcjs
 
-const { createWaterDropDataGenerator } = xydata;
+const { createWaterDropDataGenerator } = xydata
 
-// Create a XY Chart 
+// Create a XY Chart
 const chart = lightningChart({
     // Example data set is a bit larger than actual displayed heatmap. This is intentional, but displays a console warning.
     // This flag is specified to omit this warning.
-    warnings: false
+    warnings: false,
 })
-  .ChartXY({
-    disableAnimations: true,
-    // theme: Themes.darkGold
-  })
-  .setTitle('Loading temperature data ...')
-  .setPadding({ right: 40 })
-  .setMouseInteractions(false)
+    .ChartXY({
+        // theme: Themes.darkGold
+    })
+    .setTitle('Loading temperature data ...')
+    .setPadding({ right: 40 })
+    .setMouseInteractions(false)
 
-const axisY  = chart.getDefaultAxisY()
-  .setTitle('Latitude')
-  .setMouseInteractions(false)
-
-const axisX = chart.getDefaultAxisX()
-  .setTitle('Longitude')
-  .setMouseInteractions(false)
+const axisY = chart.getDefaultAxisY().setTitle('Latitude').setMouseInteractions(false).setAnimationScroll(false)
+const axisX = chart.getDefaultAxisX().setTitle('Longitude').setMouseInteractions(false).setAnimationScroll(false)
+const theme = chart.getTheme()
 
 // Define value -> color look up table.
+let legend
 const setHeatmapPalette = (transparency) => {
-  heatmap.setFillStyle(new PalettedFill({
-    lut: new LUT({
-      units: 'intensity',
-      steps: [
-        // NOTE: Units in Fahrenheits.
-        { value: 0, color: ColorRGBA(255, 255, 255, 0) },
-        { value: 5, color: ColorRGBA(208, 0, 255, transparency) },
-        { value: 20, color: ColorRGBA(10, 190, 255, transparency) },
-        { value: 40, color: ColorRGBA(10, 10, 255, transparency) },
-        { value: 50, color: ColorRGBA(9, 255, 0, transparency) },
-        { value: 60, color: ColorRGBA(251, 255, 0, transparency) },
-        { value: 70, color: ColorRGBA(255, 132, 0, transparency) },
-        { value: 80, color: ColorRGBA(255, 38, 0, transparency) },
-        { value: 100, color: ColorRGBA(200, 0, 0, transparency) },
-      ],
-      interpolate: true,
-    })
-  }))
+    heatmap.setFillStyle(
+        new PalettedFill({
+            lut: new LUT({
+                units: 'intensity',
+                steps: regularColorSteps(0, 100, theme.examples.coldHotColorPalette, { alpha: transparency }),
+                interpolate: true,
+            }),
+        }),
+    )
+    legend = legend || chart.addLegendBox().add(chart)
 }
 
 // Add Heatmap Series
 const columns = 500
 const rows = 320
 const heatmap = chart
-  .addHeatmapGridSeries({
-      columns,
-      rows,
-      start: { x: -126, y: 24 },
-      end: { x: -66, y: 48.00 },
-      dataOrder: 'columns',
-  })
-  .setWireframeStyle(emptyLine)
-  // Customize cursor result table formatting.
-  .setCursorResultTableFormatter((builder, _, dataPoint) => builder
-    .addRow(formatLongitudeLatitude(dataPoint.x, dataPoint.y))
-    .addRow('Temp:','', fahrenheitToCelsius(dataPoint.intensity).toFixed(1), '\u00B0С')
-  )
+    .addHeatmapGridSeries({
+        columns,
+        rows,
+        start: { x: -126, y: 24 },
+        end: { x: -66, y: 48.0 },
+        dataOrder: 'columns',
+    })
+    .setWireframeStyle(emptyLine)
+    // Customize cursor result table formatting.
+    .setCursorResultTableFormatter((builder, _, dataPoint) =>
+        builder
+            .addRow(formatLongitudeLatitude(dataPoint.x, dataPoint.y))
+            .addRow('Temp:', '', fahrenheitToCelsius(dataPoint.intensity).toFixed(1), '\u00B0С'),
+    )
 
 // Get Temperature Data
-fetch( document.head.baseURI + 'examples/assets/1110/usa-temperature-data.json' )
-  .then(r => r.json())
-  .then(temperatureData => {
-    // Add data to the Heatmap Series
-    heatmap.invalidateIntensityValues(temperatureData.data)
-    chart.setTitle('Loading animation ...')
+fetch(document.head.baseURI + 'examples/assets/1110/usa-temperature-data.json')
+    .then((r) => r.json())
+    .then((temperatureData) => {
+        // Add data to the Heatmap Series
+        heatmap.invalidateIntensityValues(temperatureData.data)
+        chart.setTitle('Loading animation ...')
 
-    // Generate random offset data set for real-time animation.
-    createWaterDropDataGenerator()
-      .setColumns(columns)
-      .setRows(rows)
-      .setWaterDrops([
-        { columnNormalized: .6, rowNormalized: .6, amplitude: 1 },
-        { columnNormalized: .2, rowNormalized: .1, amplitude: .9 },
-        { columnNormalized: .1, rowNormalized: .8, amplitude: .6 },
-      ])
-      .generate()
-      .then(randomData => {
-        // Map random data to more realistic fahrenheit value range.
-        return randomData.map(row => row.map(fahrenheitOffset => fahrenheitOffset * .40))
-      })
-      .then(randomData => {
-        chart.setTitle('Animated United States Temperature Heat map')
+        // Generate random offset data set for real-time animation.
+        createWaterDropDataGenerator()
+            .setColumns(columns)
+            .setRows(rows)
+            .setWaterDrops([
+                { columnNormalized: 0.6, rowNormalized: 0.6, amplitude: 1 },
+                { columnNormalized: 0.2, rowNormalized: 0.1, amplitude: 0.9 },
+                { columnNormalized: 0.1, rowNormalized: 0.8, amplitude: 0.6 },
+            ])
+            .generate()
+            .then((randomData) => {
+                // Map random data to more realistic fahrenheit value range.
+                return randomData.map((row) => row.map((fahrenheitOffset) => fahrenheitOffset * 0.4))
+            })
+            .then((randomData) => {
+                chart.setTitle('Animated United States Temperature Heat map')
 
-        // Generate n data sets that are interpolations between temperature data set and offset with random data.
-        const animationSteps = 100
-        const dataSets = []
-        for (let i = 0; i < animationSteps; i += 1) {
-          // [0, 1]
-          const interpolation = (i) / (animationSteps - 1)
-          if (interpolation === 0) {
-            dataSets.push(temperatureData.data)
-          } else {
-            const interpolatedDataSet = []
-            for (let column = 0; column < columns; column += 1) {
-              const columnValues = []
-              interpolatedDataSet.push(columnValues)
-              for (let row = 0; row < rows; row += 1) {
-                columnValues.push(temperatureData.data[column][row] + interpolation * randomData[row][column])
-              }
-            }
-            dataSets.push(interpolatedDataSet)
-          }
-        }
+                // Generate n data sets that are interpolations between temperature data set and offset with random data.
+                const animationSteps = 100
+                const dataSets = []
+                for (let i = 0; i < animationSteps; i += 1) {
+                    // [0, 1]
+                    const interpolation = i / (animationSteps - 1)
+                    if (interpolation === 0) {
+                        dataSets.push(temperatureData.data)
+                    } else {
+                        const interpolatedDataSet = []
+                        for (let column = 0; column < columns; column += 1) {
+                            const columnValues = []
+                            interpolatedDataSet.push(columnValues)
+                            for (let row = 0; row < rows; row += 1) {
+                                columnValues.push(temperatureData.data[column][row] + interpolation * randomData[row][column])
+                            }
+                        }
+                        dataSets.push(interpolatedDataSet)
+                    }
+                }
 
-        // Animate continuous interpolation between temperature data set and offset with random data.
-        let dAnimation = 0
-        let tPrevAnimationUpdate = window.performance.now()
-        let activeDataSetIndex = 0
-        const animationDurationMs = 5000
-        const updateAnimation = () => {
-          const tNow = window.performance.now()
-          const dDelta = tNow - tPrevAnimationUpdate
-          dAnimation += dDelta
-          // Calculate animation position as [0, 1] where 0 = exact temperature data and 1 = exact temperature + random data.
-          const a = dAnimation % animationDurationMs
-          const animationPosition = a < animationDurationMs / 2 ?
-            a / (animationDurationMs / 2) :
-            1 - ((a - animationDurationMs / 2) / (animationDurationMs / 2))
+                // Animate continuous interpolation between temperature data set and offset with random data.
+                let dAnimation = 0
+                let tPrevAnimationUpdate = window.performance.now()
+                let activeDataSetIndex = 0
+                const animationDurationMs = 5000
+                const updateAnimation = () => {
+                    const tNow = window.performance.now()
+                    const dDelta = tNow - tPrevAnimationUpdate
+                    dAnimation += dDelta
+                    // Calculate animation position as [0, 1] where 0 = exact temperature data and 1 = exact temperature + random data.
+                    const a = dAnimation % animationDurationMs
+                    const animationPosition =
+                        a < animationDurationMs / 2
+                            ? a / (animationDurationMs / 2)
+                            : 1 - (a - animationDurationMs / 2) / (animationDurationMs / 2)
 
-          const dataSetIndex = Math.round(animationPosition * (dataSets.length - 1))
-          if (dataSetIndex !== activeDataSetIndex) {
-            activeDataSetIndex = dataSetIndex
-            // Change displayed heat map data set.
-            heatmap.invalidateIntensityValues(dataSets[dataSetIndex])
-          }
+                    const dataSetIndex = Math.round(animationPosition * (dataSets.length - 1))
+                    if (dataSetIndex !== activeDataSetIndex) {
+                        activeDataSetIndex = dataSetIndex
+                        // Change displayed heat map data set.
+                        heatmap.invalidateIntensityValues(dataSets[dataSetIndex])
+                    }
 
-          tPrevAnimationUpdate = tNow
-          requestAnimationFrame(updateAnimation)
-        }
-        updateAnimation()
-      })
-  })
+                    tPrevAnimationUpdate = tNow
+                    requestAnimationFrame(updateAnimation)
+                }
+                updateAnimation()
+            })
+    })
 
 // Load picture of USA map.
 const bgImg = new Image()
 bgImg.crossOrigin = ''
-bgImg.src = document.head.baseURI + 'examples/assets/1110/usa.png'
+bgImg.src = document.head.baseURI + `examples/assets/1110/${theme.isDark ? 'usa.png' : 'usa-light.png'}`
 
 // Style Chart series background as the picture.
 chart.setSeriesBackgroundFillStyle(
-  new ImageFill({
-    source: bgImg,
-    fitMode: ImageFitMode.Stretch,
-  }),
+    new ImageFill({
+        source: bgImg,
+        fitMode: ImageFitMode.Stretch,
+    }),
 )
 
-// Utility function for converting Fahrenheit to Celsius 
+// Utility function for converting Fahrenheit to Celsius
 const fahrenheitToCelsius = (celsius) => {
-  return (celsius - 32)*(5/9)
+    return (celsius - 32) * (5 / 9)
 }
 
 // Maintain static aspect ratio of series area.
@@ -184,37 +164,36 @@ axisX.setThickness(bottomAxisSizePx)
 axisY.setThickness(leftAxisSizePx)
 
 const updateChartAspectRatio = () => {
-  const chartBounds = chart.engine.container.getBoundingClientRect()
-  const seriesAreaSizePx = {
-    x: Math.ceil(chartBounds.width - leftAxisSizePx),
-    y: Math.ceil(chartBounds.height - bottomAxisSizePx),
-  }
+    const chartBounds = chart.engine.container.getBoundingClientRect()
+    const seriesAreaSizePx = {
+        x: Math.ceil(chartBounds.width - leftAxisSizePx),
+        y: Math.ceil(chartBounds.height - bottomAxisSizePx),
+    }
 
-  // height / width
-  const mapAspectRatio = 883 / 1600
-  const curAspectRatio = seriesAreaSizePx.y / seriesAreaSizePx.x
+    // height / width
+    const mapAspectRatio = 883 / 1600
+    const curAspectRatio = seriesAreaSizePx.y / seriesAreaSizePx.x
 
-  if (curAspectRatio < mapAspectRatio) {
-    // Add horizontal chart padding to maintain Map picture aspect ratio.
-    const targetAxisWidth = seriesAreaSizePx.y / mapAspectRatio
-    const horizontalPadding = Math.max(seriesAreaSizePx.x - targetAxisWidth, 0)
-    chart.setPadding({ left: horizontalPadding / 2, right: horizontalPadding / 2, top: 0, bottom: 0 })
-  } else if (curAspectRatio > mapAspectRatio) {
-    // Add vertical chart padding to maintain Map picture aspect ratio.
-    const targetAxisHeight = seriesAreaSizePx.x * mapAspectRatio
-    const verticalPadding = Math.max(seriesAreaSizePx.y - targetAxisHeight, 0)
-    chart.setPadding({ top: verticalPadding / 2, bottom: verticalPadding / 2, left: 0, right: 16 })
-  }
+    if (curAspectRatio < mapAspectRatio) {
+        // Add horizontal chart padding to maintain Map picture aspect ratio.
+        const targetAxisWidth = seriesAreaSizePx.y / mapAspectRatio
+        const horizontalPadding = Math.max(seriesAreaSizePx.x - targetAxisWidth, 0)
+        chart.setPadding({ left: horizontalPadding / 2, right: horizontalPadding / 2, top: 0, bottom: 0 })
+    } else if (curAspectRatio > mapAspectRatio) {
+        // Add vertical chart padding to maintain Map picture aspect ratio.
+        const targetAxisHeight = seriesAreaSizePx.x * mapAspectRatio
+        const verticalPadding = Math.max(seriesAreaSizePx.y - targetAxisHeight, 0)
+        chart.setPadding({ top: verticalPadding / 2, bottom: verticalPadding / 2, left: 0, right: 16 })
+    }
 }
 updateChartAspectRatio()
 
 // Maintain chart aspect ratio when window is resized.
 window.addEventListener('resize', updateChartAspectRatio)
 
-
 // Add slider user interface for adjusting heat map transparency.
 const exampleContainer = document.getElementById('chart') || document.body
-const uiDiv = document.createElement("div");
+const uiDiv = document.createElement('div')
 chart.engine.container.append(uiDiv)
 uiDiv.style.position = 'absolute'
 uiDiv.style.left = '6px'
@@ -223,15 +202,16 @@ uiDiv.style.zIndex = '999'
 uiDiv.style.color = 'white'
 uiDiv.style.display = 'flex'
 uiDiv.style.flexDirection = 'row'
-uiDiv.style.color = exampleContainer.parentElement.parentElement && window.getComputedStyle(exampleContainer.parentElement.parentElement).color || 'white'
-const label = document.createElement("span");
+uiDiv.style.color =
+    (exampleContainer.parentElement.parentElement && window.getComputedStyle(exampleContainer.parentElement.parentElement).color) || 'white'
+const label = document.createElement('span')
 uiDiv.append(label)
 label.innerHTML = 'Heat map transparency'
-const slider = document.createElement("input");
-uiDiv.append(slider);
-slider.type = "range";
-slider.min = 0;
-slider.max = 255;
-slider.value = 100;
+const slider = document.createElement('input')
+uiDiv.append(slider)
+slider.type = 'range'
+slider.min = 0
+slider.max = 255
+slider.value = 150
 setHeatmapPalette(slider.value)
 slider.oninput = (e) => setHeatmapPalette(slider.value)
